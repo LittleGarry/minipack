018113e1794eac06a240c051551dae0465281ff2
LittleGarry <bulgakov_egor@outlook.com>
2019-03-24 14:37:19 +0300

    Extract dependencies from a file

--- minipack.js
+++ minipack.js
@@ -1,7 +1,37 @@
 const fs = require('fs');
+const path = require('path');
+const babylon = require('babylon');
+const traverse = require('babel-traverse').default;
+const {transformFromAst} = require('babel-core');
 
-let foo = () => {
-    console.log('Everything is working!')
+let ID = 0;
+
+let createAsset = filename => {
+    const content = fs.readFileSync(filename, 'utf-8');
+    const ast = babylon.parse(content, {
+        sourceType: 'module'
+    });
+
+    const dependencies = [];
+
+    traverse(ast, {
+        ImportDeclaration: ({node}) => {
+            dependencies.push(node.source.value);
+        }
+    });
+
+    const id = ID++;
+
+    const {code} = transformFromAst(ast, null, {
+        presets: ['env']
+    });
+
+    return {
+        id,
+        filename,
+        dependencies,
+        code
+    };
 }
 
-foo();
+createAsset('minipack.js');  
